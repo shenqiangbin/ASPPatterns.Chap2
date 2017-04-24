3c7c539c9dfa931e2e50d457ac41d7eefa9a6d12
shenqiangbin <shenqiangbin@163.com>
2017-04-24 22:22:34 +0800

    最简单的结构处理

---
+++ ASPPatterns.Chap2.Service/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPPatterns.Chap2.Service
+{
+    public class ProductRepository
+    {
+        public IList<Product> GetAllProductsIn(int categoryId)
+        {
+            IList<Product> products = new List<Product>();
+
+            return products;
+        }
+    }
+}
---
+++ ASPPatterns.Chap2.Service/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPPatterns.Chap2.Service
+{
+    public class ProductService
+    {
+        private ProductRepository _productCategory;
+
+        public ProductService()
+        {
+            _productCategory = new ProductRepository();
+        }
+
+        public IList<Product> GetAllProductsIn(int categoryId)
+        {
+            return _productCategory.GetAllProductsIn(categoryId);
+        }
+    }
+}
