465e5ca27fd21399c82c6f6be7af33eb062b20b5
shenqiangbin <shenqiangbin@163.com>
2017-04-24 22:38:07 +0800

    添加缓存

--- ASPPatterns.Chap2.Service/ProductService.cs
+++ ASPPatterns.Chap2.Service/ProductService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace ASPPatterns.Chap2.Service
 {
@@ -17,7 +18,17 @@ namespace ASPPatterns.Chap2.Service
 
         public IList<Product> GetAllProductsIn(int categoryId)
         {
-            return _productCategory.GetAllProductsIn(categoryId);
+            IList<Product> products = null;
+
+            string storageKey = $"products_in_categoryId_{categoryId}";
+            products = HttpContext.Current.Cache.Get(storageKey) as IList<Product>;
+            if (products == null)
+            {
+                products = _productCategory.GetAllProductsIn(categoryId);
+                HttpContext.Current.Cache.Insert(storageKey, products);
+            }
+
+            return products;
         }
     }
 }
